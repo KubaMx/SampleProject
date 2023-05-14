a6b0074b8ecda6237e164189b36837147276a85c
student9 <ml>
2023-05-14 16:00:43 +0200

    delete redundant method

--- src/main/java/BoardPrinter.java
+++ src/main/java/BoardPrinter.java
@@ -6,26 +6,10 @@ public class BoardPrinter {
     public static void main(String[] args) {
       //  List<String> board = new ArrayList<>();
 
-        printEmtpySudoku();
-        board[1][1] = 5;
-        printSudokuWithNumber();
+        printSudoku();
     }
 
-    private static void printEmtpySudoku() {
-        for (int row = 0; row < 9; row++) {
-            for (int column = 0; column < 9; column++) {
-                System.out.print("_ ");
-                if (column == 2 || column == 5) System.out.print("* ");
-            }
-            System.out.println();
-            if (row == 2 || row == 5) {
-                System.out.println("*********************");
-            }
-        }
-        System.out.println();
-    }
-
-    private static void printSudokuWithNumber() {
+    private static void printSudoku() {
         for (int row = 0; row < 9; row++) {
             for (int column = 0; column < 9; column++) {
                 if (board[row][column] == 0)
